4dceba668ea6c0a10b5dab6f5912041a3e18d6e5
juechen-zzz <240553516@qq.com>
2019-02-01 17:14:57 +0800

    2019-2-1

---
+++ python/LeetCode/number_1.py
@@ -0,0 +1,15 @@
+def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+    d = {}
+    for i, n in enumerate(nums):
+        m = target - n
+        if m in d:
+            return [d[m], i]
+        else:
+            d[n] = i
+        
+        
