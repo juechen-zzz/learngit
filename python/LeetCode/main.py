aa49a7602e3647faf4d88e0fa0725385fbae145f
juechen-zzz <240553516@qq.com>
2019-04-12 15:20:55 +0800

    del something

--- python/LeetCode/main.py
+++
@@ -1,6 +0,0 @@
-from LeetCode.number_1 import twoSum
-
-nums = [2, 7, 11, 15]
-target = 9
-[a, b] = twoSum(nums, target)
-print([a, b])
--- python/LeetCode/number_1.py
+++
@@ -1,8 +0,0 @@
-def twoSum(nums, target):
-    d = {}
-    for i, n in enumerate(nums):
-        m = target - n
-        if m in d:
-            return [d[m], i]
-        else:
-            d[n] = i
