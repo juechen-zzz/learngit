51d958ffd98dc8bbbd4ffb60ac03ead778a1f172
juechen-zzz <240553516@qq.com>
2019-06-11 08:39:01 +0800

    start sorts

---
+++ Algorithm/Sorts/sorts.py
@@ -0,0 +1,65 @@
+"""
+    冒泡、插入、选择排序
+    Bubble sort, insertion sort, selection sort
+"""
+
+from typing import List
+
+# bubble sort
+def bubble_sort(a:List[int]):
+    length = len(a)
+    if length <= 1:
+        return
+
+    for i in range(length):
+        made_swap = False
+        for j in range(length - i - 1):
+            if a[j] > a[j+1]:
+                a[j], a[j+1] = a[j+1], a[j]
+                made_swap = True
+        if not made_swap:
+            break
+
+# insertion sort
+def insertion_sort(a):
+    length = len(a)
+    if length <= 1:
+        return
+
+    for i in range(1, length):
+        value = a[i]
+        j = i - 1
+        while j >= 0 and a[j] > value:
+            a[j+1] = a[j]
+            j -= 1
+        a[j+1] = value
+
+# selection sort
+def selection_sort(a):
+    length = len(a)
+    if length <= 1:
+        return
+
+    for i in range(length):
+        min_index = i
+        min_val = a[i]
+        for j in range(i, length):
+            if a[j] < min_val:
+                min_val = a[j]
+                min_index = j
+        a[i], a[min_index] = a[min_index], a[i]
+
+
+
+if __name__ == "__main__":
+    array = [5, 6, -1, 4, 2, 8, 10, 7, 6]
+    bubble_sort(array)
+    print(array)
+
+    array = [5, 6, -1, 4, 2, 8, 10, 7, 6]
+    insertion_sort(array)
+    print(array)
+
+    array = [5, 6, -1, 4, 2, 8, 10, 7, 6]
+    selection_sort(array)
+    print(array)
