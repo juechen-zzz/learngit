fa6eec6e69f001c14a23d39696834b0f7186b658
juechen-zzz <240553516@qq.com>
2019-09-20 10:40:25 +0800

    add virtual form

--- python/tensorflow_test/virtual_form_2.py
+++ python/tensorflow_test/virtual_form_2.py
@@ -1,5 +1,5 @@
 """
-    可以看到训练过程
+    可以看到训练过程，可视化
 """
 
 import tensorflow as tf
@@ -21,25 +21,25 @@ with tf.variable_scope('Net'):
     l1 = tf.layers.dense(tf_x, 10, tf.nn.relu, name='hidden_layer')
     output = tf.layers.dense(l1, 1, name='output_layer')
 
-    # add to histogram summary
+    # add to histogram summary（画隐藏和输出层的结果图）
     tf.compat.v1.summary.histogram('h_out', l1)
     tf.compat.v1.summary.histogram('pred', output)
 
 loss = tf.compat.v1.losses.mean_squared_error(tf_y, output, scope='loss')
 train_op = tf.compat.v1.train.GradientDescentOptimizer(learning_rate=0.5).minimize(loss)
 
-tf.compat.v1.summary.scalar('loss', loss)     # add loss to scalar summary
+tf.compat.v1.summary.scalar('loss', loss)     # add loss to scalar summary（画损失函数的迭代图）
 
 sess = tf.compat.v1.Session()
 sess.run(tf.compat.v1.global_variables_initializer())
 
-writer = tf.compat.v1.summary.FileWriter('./log', sess.graph)     # write to file
-merge_op = tf.compat.v1.summary.merge_all()                       # operation to merge all summary
+writer = tf.compat.v1.summary.FileWriter('./log', sess.graph)     # write to file（写文件）
+merge_op = tf.compat.v1.summary.merge_all()                       # operation to merge all summary（整合所有图）
 
 for step in range(100):
     # train and net output
     _, result = sess.run([train_op, merge_op], {tf_x: x, tf_y: y})
-    writer.add_summary(result, step)
+    writer.add_summary(result, step)                              # 同训练一起迭代
 
 # Lastly, in your terminal or CMD, type this :
 # $ tensorboard --logdir path/to/log
--- python/tensorflow_test/virtual_form_3.py
+++ python/tensorflow_test/virtual_form_3.py
@@ -1,16 +1,21 @@
 """
-    将tensorflow代码进行可视化，看到模型结构，进行可视化的训练
+    可视化训练过程
 """
 
 import tensorflow as tf
+import numpy as np
 
 
-def add_layer(inputs, in_size, out_size, activation_function=None):
-    with tf.name_scope('layer'):
+def add_layer(inputs, in_size, out_size, n_layer, activation_function=None):
+    layer_name = 'layer%s' % n_layer
+    with tf.name_scope(layer_name):
         with tf.name_scope('weight'):
             Weights = tf.Variable(tf.random.normal([in_size, out_size]), name='W')
+            # 加入
+            tf.compat.v1.summary.histogram(layer_name + '/weights', Weights)
         with tf.name_scope('biases'):
             biases = tf.Variable(tf.zeros([1, out_size]) + 0.1, name='b')
+            tf.compat.v1.summary.histogram(layer_name + '/biases', biases)
         with tf.name_scope('Wx_plus_b'):
             Wx_plus_b = tf.add(tf.matmul(inputs, Weights), biases)
 
@@ -18,29 +23,42 @@ def add_layer(inputs, in_size, out_size, activation_function=None):
             outputs = Wx_plus_b
         else:
             outputs = activation_function(Wx_plus_b)
+        tf.compat.v1.summary.histogram(layer_name + '/outputs', outputs)
         return outputs
 
+x_data = np.linspace(-1, 1, 300)[:, np.newaxis]
+noise = np.random.normal(0, 0.05, x_data.shape)
+y_data = np.square(x_data) - 0.5 + noise
 
-# 定义名字的象征inputs
 with tf.name_scope('inputs'):
     xs = tf.compat.v1.placeholder(tf.float32, [None, 1], name='x_input')
     ys = tf.compat.v1.placeholder(tf.float32, [None, 1], name='y_input')
 
 # add hidden layer
-l_1 = add_layer(xs, 1, 10, activation_function=tf.nn.relu)
+l_1 = add_layer(xs, 1, 10, n_layer=1, activation_function=tf.nn.relu)
 # add output layer
-prediction = add_layer(l_1, 10, 1, activation_function=None)
+prediction = add_layer(l_1, 10, 1, n_layer=2, activation_function=None)
+
 
 # the error
 with tf.name_scope('loss'):
     loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys - prediction), reduction_indices=[1]))
+    tf.compat.v1.summary.scalar('loss', loss)
 with tf.name_scope('train'):
     train_step = tf.compat.v1.train.GradientDescentOptimizer(0.1).minimize(loss)
 
+
 sess = tf.compat.v1.Session()
 
-# 最重要的一步，写出总结，加载到一个文件中
-# $ tensorboard --logdir log    先转到log之上的那个目录
+merged = tf.compat.v1.summary.merge_all()
 writer = tf.compat.v1.summary.FileWriter('./log', sess.graph)
 
 sess.run(tf.compat.v1.global_variables_initializer())
+
+for i in range(1000):
+    sess.run(train_step, feed_dict={xs: x_data, ys: y_data})
+    if i % 50 == 0:
+        result = sess.run(merged, feed_dict={xs: x_data, ys: y_data})
+        writer.add_summary(result, i)
+
+# $ tensorboard --logdir log    先转到log之上的那个目录
